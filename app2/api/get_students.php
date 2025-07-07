afef32efb34455ffd887c921830ce90a2c2e7fe1
yn <jenny783113@gmail.com>
2025-07-07 11:34:15 +0800

    (t commit:將學生資料以表格方式呈現在網頁上)

--- app2/api/get_students.php
+++ app2/api/get_students.php
@@ -4,6 +4,11 @@ $classroom=$_GET['classroom']??'101';
 
 $students=$Stu->all(['classroom'=>$classroom]);
 
+foreach($students as $key=>$student){
+    $students[$key]['classname']=$ref[$student['classroom']];
+
+}
+
 
 header('Content-type: application/json; charset=utf-8;');
 echo json_encode($students);
