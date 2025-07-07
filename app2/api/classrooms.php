6ec4d5fef23c2204e3f82274186a4ca1870e1f9b
yn <jenny783113@gmail.com>
2025-07-07 10:37:41 +0800

    (t commit:將班級編碼在後端轉成中文(&美化))

--- app2/api/classrooms.php
+++ app2/api/classrooms.php
@@ -3,6 +3,23 @@
 $sql="select classroom from students group by classroom";
 $classrooms=q($sql);
 
+$ref=[
+    '101'=>'一年一班',
+    '102'=>'一年二班',
+    '103'=>'一年三班',
+    '104'=>'一年四班',
+    '105'=>'一年五班',
+    '106'=>'一年六班',
+    '107'=>'一年七班',
+    '108'=>'一年八班',
+    '109'=>'一年九班',
+    '110'=>'一年十班',
+];
+
+foreach($classrooms as $key=>$classroom){
+    $classrooms[$key]['classroom']=$ref[$classroom['classroom']];
+
+}
 
 header('Content-type: application/json; charset=utf-8;');
 echo json_encode($classrooms);
